0bbd2a7e361362e2902b50a3cac455dca611712f
anle10 <hongan101003@gmail.com>
2025-11-23 00:53:34 +0200

    JPA repository for transaction

--- src/main/java/fi/haagahelia/financemanager/transaction/TransactionRepository.java
+++ src/main/java/fi/haagahelia/financemanager/transaction/TransactionRepository.java
@@ -1,5 +1,13 @@
 package fi.haagahelia.financemanager.transaction;
 
-public class TransactionRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+/**
+ * JPA repository for accessing Transaction data.
+ */
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountId(Long accountId);
 }

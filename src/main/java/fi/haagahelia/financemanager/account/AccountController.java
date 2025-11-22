a63ce8f59d2a576c81b7a45baf1147322b209b6f
anle10 <hongan101003@gmail.com>
2025-11-23 00:22:48 +0200

    REST API for managing accounts of the logged-in user

--- src/main/java/fi/haagahelia/financemanager/account/AccountController.java
+++ src/main/java/fi/haagahelia/financemanager/account/AccountController.java
@@ -1,5 +1,42 @@
 package fi.haagahelia.financemanager.account;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * REST API for managing accounts of the logged-in user.
+ */
+@RestController
+@RequestMapping("/api/accounts")
 public class AccountController {
     
-}
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    /**
+     * GET /api/accounts
+     *
+     * Returns all accounts belonging to the current user.
+     */
+    @GetMapping
+    public List<AccountDto> getMyAccounts() {
+        return accountService.getMyAccounts();
+    }
+
+    /**
+     * POST /api/accounts
+     *
+     * Creates a new account for the current user.
+     */
+    @PostMapping
+    public ResponseEntity<AccountDto> createAccount(@RequestBody AccountDto dto) {
+        AccountDto created = accountService.createAccount(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    }
+ }

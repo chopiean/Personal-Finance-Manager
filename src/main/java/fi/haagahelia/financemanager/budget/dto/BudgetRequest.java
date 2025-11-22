37aaf39924c1132a9495e981be28a86b9e139d47
anle10 <hongan101003@gmail.com>
2025-11-23 01:17:49 +0200

    Incoming JSON payload for creating a Budget

--- src/main/java/fi/haagahelia/financemanager/budget/dto/BudgetRequest.java
+++ src/main/java/fi/haagahelia/financemanager/budget/dto/BudgetRequest.java
@@ -1,5 +1,30 @@
 package fi.haagahelia.financemanager.budget.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+/**
+ * Incoming JSON payload for creating a Budget.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BudgetRequest {
     
+    @NotNull
+    private String category;
+
+    @NotNull
+    private Double limitAmount;
+
+    @NotNull
+    private Long accountId;
+
+    @NotNull
+    private int month;
+
+    @NotNull
+    private int year;
 }

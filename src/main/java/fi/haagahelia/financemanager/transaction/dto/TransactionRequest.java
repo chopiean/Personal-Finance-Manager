44b01094d373b6ee5809f61888c871ee0a83fa8e
anle10 <hongan101003@gmail.com>
2025-11-23 00:50:22 +0200

    Creating or updating a transaction

--- src/main/java/fi/haagahelia/financemanager/transaction/dto/TransactionRequest.java
+++ src/main/java/fi/haagahelia/financemanager/transaction/dto/TransactionRequest.java
@@ -1,5 +1,21 @@
 package fi.haagahelia.financemanager.transaction.dto;
 
+import fi.haagahelia.financemanager.transaction.TransactionType;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * Incoming request for creating or updating a transaction.
+ */
+@Data
 public class TransactionRequest {
     
+    private String description;
+    private double amount;
+    private LocalDate date;
+    private TransactionType type;
+
+    // ID of the account the transaction belongs to
+    private Long accountId;
 }

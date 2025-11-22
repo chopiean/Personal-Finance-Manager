98c35371dddebca09309b93c420303d9a571d050
anle10 <hongan101003@gmail.com>
2025-11-23 00:52:08 +0200

    Transaction response model used to return transaction data to the client

--- src/main/java/fi/haagahelia/financemanager/transaction/dto/TransactionResponse.java
+++ src/main/java/fi/haagahelia/financemanager/transaction/dto/TransactionResponse.java
@@ -1,5 +1,24 @@
 package fi.haagahelia.financemanager.transaction.dto;
 
+import fi.haagahelia.financemanager.transaction.TransactionType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+/**
+ * Outgoing response model used to return transaction data to the client.
+ */
+@Data
+@Builder
 public class TransactionResponse {
     
+    private Long id;
+    private String description;
+    private double amount;
+    private LocalDate date;
+    private TransactionType type;
+
+    private Long accounts;
+    private String accountName;
 }

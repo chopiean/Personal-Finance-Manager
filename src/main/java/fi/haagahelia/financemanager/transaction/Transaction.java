c57c4a278279a4ef4492554082a1898326843688
anle10 <hongan101003@gmail.com>
2025-11-23 00:44:19 +0200

    Create Transaction Type and represents a financial transaction

--- src/main/java/fi/haagahelia/financemanager/transaction/Transaction.java
+++ src/main/java/fi/haagahelia/financemanager/transaction/Transaction.java
@@ -1,5 +1,36 @@
 package fi.haagahelia.financemanager.transaction;
 
+import fi.haagahelia.financemanager.account.Account;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+/**
+ * Represents a financial transaction.
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Transaction {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private double amount;
+
+    private LocalDate date;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType type;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
 }
---
+++ src/main/java/fi/haagahelia/financemanager/transaction/TransactionType.java
@@ -0,0 +1,10 @@
+package fi.haagahelia.financemanager.transaction;
+
+/**
+ * Type of financial transaction.
+ */
+public enum TransactionType {
+    INCOME,
+    EXPENSE,
+    TRANSFER
+}

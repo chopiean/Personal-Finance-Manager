06bc52725fdd849fb760b53b5c5a78099fef07fb
anle10 <hongan101003@gmail.com>
2025-11-22 23:55:00 +0200

    Clarify account types

---
+++ src/main/java/fi/haagahelia/financemanager/account/AccountType.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.financemanager.account;
+
+/**
+ * Supported account types in the personal finance manager.
+ */
+public enum AccountType {
+    CASH,
+    BANK,
+    CREDIT_CARD,
+    SAVINGS,
+    INVESTMENT,
+    OTHER
+}

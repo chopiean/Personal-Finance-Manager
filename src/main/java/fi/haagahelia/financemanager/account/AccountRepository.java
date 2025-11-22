9bebacc813708887d23d201752120adcb52fb25d
anle10 <hongan101003@gmail.com>
2025-11-23 00:02:04 +0200

    JPA repository for Account entity

--- src/main/java/fi/haagahelia/financemanager/account/AccountRepository.java
+++ src/main/java/fi/haagahelia/financemanager/account/AccountRepository.java
@@ -1,5 +1,13 @@
 package fi.haagahelia.financemanager.account;
 
-public class AccountRepository {
-    
+import fi.haagahelia.financemanager.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * JPA repository for Account entity.
+ */
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUser(User user);
 }

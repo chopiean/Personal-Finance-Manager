5346a45d959293b1feabb754cccbc112fe8c11f1
anle10 <hongan101003@gmail.com>
2025-11-24 14:00:34 +0200

    Create Dashboard page

---
+++ frontend/src/pages/dashboard/DashboardPage.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../../components/Navbar";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <Navbar />
+      <div style={{ padding: 20 }}>
+        <h1>Dashboard</h1>
+        <p>Welcome to your personal finance manager!</p>
+      </div>
+    </>
+  );
+}
